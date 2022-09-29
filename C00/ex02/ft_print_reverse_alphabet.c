94dcd1ff005c3894433b6874cdca22f83d7b39f5
Felix Georgeault <fgeorgea@fu-r2-p5.s19.be>
2022-09-29 17:46:39 +0200

    rm cached git nested

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgeorgea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 17:27:26 by fgeorgea          #+#    #+#             */
+/*   Updated: 2022/09/09 10:32:33 by fgeorgea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+void	ft_putchar(char c);
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	int		i;
+
+	i = 122;
+	while (i >= 97)
+	{
+		ft_putchar (i);
+		i--;
+	}
+}
---
+++ C00/ex03/ft_print_numbers.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgeorgea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 18:44:49 by fgeorgea          #+#    #+#             */
+/*   Updated: 2022/09/09 10:34:53 by fgeorgea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void);
+void	ft_putchar(char c);
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	int		i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		ft_putchar(i);
+		i++;
+	}
+}
